a2230461cd3e87d0c5521714f41f0b26b1f97018
Hashini Divyanjanee <143160465+HashiniDivyanjanee@users.noreply.github.com>
2025-08-22 10:06:35 +0530

    first commit

---
+++ lib/features/banner_slider/data/data.dart
@@ -0,0 +1,5 @@
+final List<String> bannerSlider = [
+  'assets/images/1.jpg',
+  'assets/images/2.jpg',
+  'assets/images/5.jpg',
+];
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:ecommerce_app/features/banner_slider/presentation/page/banner_slider.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: BannerSlider(),
+    );
+  }
+}
